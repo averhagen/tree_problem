31986cbd1ce339f1550e8c84380aa110de9be42e
amverhagen <averhagen@users.noreply.github.com>
2018-12-18 15:39:51 -0500

    Add console.log to show results of tree growing.

--- tree-problem.js
+++ tree-problem.js
@@ -20,15 +20,22 @@ const Spring = new GrowthSeason("Spring", springGrowthFunction);
 const GROWTH_PATTEN = [Fall, Spring];
 
 function growTreeForSeasons(startingTreeHeight, numberOfSeasons, growthPattern) {
-
     var treeHeight = startingTreeHeight;
+    var progressChart = [startingTreeHeight];
 
     for (var i = 1; i <= numberOfSeasons; i++) {
         var currentSeason = growthPattern[(i - 1) % growthPattern.length]
         treeHeight = currentSeason.growthFunction(treeHeight);
+        progressChart.push(treeHeight);
     }
-
+    console.log("Growth Results:");
+    console.log("\tStarting Height: " + startingTreeHeight);
+    console.log("\tNumber of Seasons: " + numberOfSeasons);
+    console.log("\tGrowth Progress: " + progressChart)
+    console.log("\tFinal Height: " + treeHeight + "\n");
     return treeHeight;
 }
 
-console.log(growTreeForSeasons(0,2,GROWTH_PATTEN));
+growTreeForSeasons(0,2,GROWTH_PATTEN);
+growTreeForSeasons(0,5,GROWTH_PATTEN);
+growTreeForSeasons(0,7,GROWTH_PATTEN);

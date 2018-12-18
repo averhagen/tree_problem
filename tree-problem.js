b6b19688b37b8d50d6325bc2c45e27ead18be509
amverhagen <averhagen@users.noreply.github.com>
2018-12-18 15:14:48 -0500

    Refactor white space and position of const variables in script.

--- tree-problem.js
+++ tree-problem.js
@@ -1,7 +1,6 @@
 'use-strict';
 
 class GrowthSeason {
-
     constructor(seasonName, growthFunction) {
         this.seasonName = seasonName;
         this.growthFunction = growthFunction;
@@ -12,22 +11,20 @@ function fallGrowthFunction(currentHeight) {
     return currentHeight + 1;
 }
 
-const Fall = new GrowthSeason("Fall", fallGrowthFunction);
-
 function springGrowthFunction(currentHeight) {
     return currentHeight * currentHeight;
 }
 
-const Spring = new GrowthSeason("Spring", fallGrowthFunction);
-
+const Fall = new GrowthSeason("Fall", fallGrowthFunction);
+const Spring = new GrowthSeason("Spring", springGrowthFunction);
 const GROWTH_PATTEN = [Fall, Spring];
 
 function growTreeForSeasons(startingTreeHeight, numberOfSeasons, growthPattern) {
 
     var treeHeight = startingTreeHeight;
 
-    for(var i = 1; i <= numberOfSeasons; i++) {
-        
+    for (var i = 1; i <= numberOfSeasons; i++) {
+
     }
-    
+
 }
